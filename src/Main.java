2aa79005e184c5197992564ac5eed491982e2f57
deivid <deividsantos@infojr.com.br>
2026-01-05 22:08:16 -0300

    feat: remover código desnecessário e organizar a classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import exceptions.BeneficioInvalidoException;
+import java.time.LocalDate;
+import models.Desenvolvedor;
+import models.Estagiario;
+import models.Gerente;
+import services.FuncionarioService;
+import utils.FuncionarioUtils;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        FuncionarioService service = new FuncionarioService();
+
+        Desenvolvedor dev = new Desenvolvedor(
+            "Ana", "123.456.789-00", LocalDate.of(2022, 1, 10), 3000, "Junior", new String[]{"Java", "Python"}
+        );
+        Gerente gerente = new Gerente(
+            "Carlos", "987.654.321-00", LocalDate.of(2020, 5, 20), 6000
+        );
+        Estagiario estagiario = new Estagiario(
+            "Lucas", "111.222.333-44", LocalDate.of(2023, 3, 1), 1500, 30
+        );
+
+        try {
+            dev.adicionarBeneficio("Vale Refeição");
+            gerente.adicionarBeneficio("Plano de Saúde");
+            estagiario.adicionarBeneficio(""); 
+        } catch (BeneficioInvalidoException e) {
+            System.out.println("Erro ao adicionar benefício: " + e.getMessage());
+        }
+
+        dev.promover();
+        gerente.adicionarBonusAnual(2000);
+
+        service.adicionar(dev);
+        service.adicionar(gerente);
+        service.adicionar(estagiario);
+
+        System.out.println("\n--- Funcionários cadastrados ---");
+        for (var f : service.listar()) {
+            FuncionarioUtils.imprimirDados(f);
+        }
     }
 }
